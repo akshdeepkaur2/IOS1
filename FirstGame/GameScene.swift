49418cffb54ed0443e45d067ac0089025b339dba
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-06-05 09:33:32 -0400

    add some text

--- FirstGame/GameScene.swift
+++ FirstGame/GameScene.swift
@@ -10,12 +10,13 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    
-    
     override func didMove(to view: SKView) {
-        
-        
+        let scorelabel = SKLabelNode(text: "Hola")
+        scorelabel.position = CGPoint(x:200, y: 300)
+        scorelabel.fontName = "Avenir"
+        scorelabel.fontSize = 60
+        scorelabel.fontColor = SKColor.white
+        addChild(scorelabel)
         }
         
         
